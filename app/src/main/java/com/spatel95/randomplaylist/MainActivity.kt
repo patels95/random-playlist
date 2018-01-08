7780495368e5394ba7de4341daf289f79c473b5a
Sanam Patel <patels326@gmail.com>
2018-01-07 21:34:06 -0500

    start login activity

--- app/src/main/java/com/spatel95/randomplaylist/MainActivity.kt
+++ app/src/main/java/com/spatel95/randomplaylist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.spatel95.randomplaylist
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 
 class MainActivity : Activity() {
@@ -8,5 +9,9 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // go to login activity
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
