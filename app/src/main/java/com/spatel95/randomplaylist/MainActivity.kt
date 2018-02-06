b8c2201ccfd4e2cd4adfdcb126fbed2ad4de0e77
Sanam Patel <patels326@gmail.com>
2018-02-05 23:03:54 -0500

    added getSavedSongs

--- app/src/main/java/com/spatel95/randomplaylist/MainActivity.kt
+++ app/src/main/java/com/spatel95/randomplaylist/MainActivity.kt
@@ -2,7 +2,11 @@ package com.spatel95.randomplaylist
 
 import android.app.Activity
 import android.os.Bundle
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.error
 import org.jetbrains.anko.info
 import org.jetbrains.anko.intentFor
 
@@ -14,8 +18,24 @@ class MainActivity : Activity(), AnkoLogger {
         setContentView(R.layout.activity_main)
 
         info("main activity")
-
+        // TODO: get access token from LoginActivity and set Bearer
+        FuelManager.instance.baseHeaders = mapOf("Bearer" to "")
+2
         // go to login activity
         startActivity(intentFor<LoginActivity>())
+    }
+
+    // get user's saved songs
+    private fun getSavedSongs() {
+        "https://api.spotify.com/v1/me/tracks".httpGet().responseString { request, response, result ->
+            when (result) {
+                is Result.Failure -> {
+                    error("error: " + result.get())
+                }
+                is Result.Success -> {
+                    info(result.get())
+                }
+            }
+        }
     }
 }
