cded5039b8c79d8c90a3bf63fcf6d9a90028689e
Sanam Patel <patels326@gmail.com>
2018-03-25 17:09:30 -0400

    create track data class. loop items json array

--- app/src/main/java/com/spatel95/randomplaylist/MainActivity.kt
+++ app/src/main/java/com/spatel95/randomplaylist/MainActivity.kt
@@ -6,11 +6,12 @@ import android.os.Bundle
 import com.github.kittinunf.fuel.core.FuelManager
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
-import com.github.kittinunf.result.getAs
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.error
 import org.jetbrains.anko.info
 import org.jetbrains.anko.intentFor
+import org.json.JSONArray
+import org.json.JSONObject
 
 
 class MainActivity : Activity(), AnkoLogger {
@@ -50,15 +51,19 @@ class MainActivity : Activity(), AnkoLogger {
 
     // get user's saved songs
     private fun getSavedSongs() {
-        "https://api.spotify.com/v1/me/tracks".httpGet().responseString { request, response, result ->
+        "https://api.spotify.com/v1/me/tracks".httpGet(listOf("limit" to "50")).responseString { request, response, result ->
             info(request.toString())
             when (result) {
                 is Result.Failure -> {
                     error("error: " + result.toString())
                 }
                 is Result.Success -> {
-                    info(result.get())
-                    // TODO: Use result
+                    val json = JSONObject(result.get())
+                    val jsonTracks = json.getJSONArray("items")
+                    (0..(jsonTracks.length() - 1))
+                            .map { jsonTracks.getJSONObject(it) }
+                            .forEach { info(it.getString("added_at")) }
+                    // TODO: Use gson to make array of Tracks
                 }
             }
         }
---
+++ app/src/main/java/com/spatel95/randomplaylist/model/Track.kt
@@ -0,0 +1,5 @@
+package com.spatel95.randomplaylist.model
+
+data class Track(val id: String, val name: String, val uri: String) {
+
+}
